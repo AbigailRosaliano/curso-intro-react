f2246e7dc05934ec7ec912964b1318231dfdebcf
AbigailRosaliano <rosaliano_abigail20@outlook.com>
2022-03-08 11:31:53 -0600

    Updates

---
+++ precache-manifest.38b7846ac640c559b2b1c0d3727505bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30ee0e3e422dcacda8b3",
+    "url": "/curso-intro-react/static/css/main.980bd3db.chunk.css"
+  },
+  {
+    "revision": "30ee0e3e422dcacda8b3",
+    "url": "/curso-intro-react/static/js/main.30ee0e3e.chunk.js"
+  },
+  {
+    "revision": "ceefb4fbb137ac8ec744",
+    "url": "/curso-intro-react/static/js/1.ceefb4fb.chunk.js"
+  },
+  {
+    "revision": "8f31ac8995903ac72b62",
+    "url": "/curso-intro-react/static/js/runtime~main.8f31ac89.js"
+  },
+  {
+    "revision": "62b6984de3e0221171bf246c5e0b3c4d",
+    "url": "/curso-intro-react/static/media/check.62b6984d.svg"
+  },
+  {
+    "revision": "b9717c6f3ab6c3d5fca0cd2dcef70f89",
+    "url": "/curso-intro-react/static/media/delete.b9717c6f.svg"
+  },
+  {
+    "revision": "d8e296db872d64566a5e61a0eb24a7c7",
+    "url": "/curso-intro-react/index.html"
+  }
+];
